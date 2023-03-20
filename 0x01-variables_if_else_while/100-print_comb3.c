22ab36b8f5f9571f36ef9137be86e474b5a70e02
Yusuf <yusluv1708@gmail.com>
2023-03-20 15:52:59 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Different combination of two digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	int b;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (b = 1; b < 10; b++)
+		{
+
+			if (n < b && n != b)
+			{
+				putchar(n + '0');
+				putchar(b + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

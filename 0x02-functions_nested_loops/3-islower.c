149034f2a85d805fb607974481561dd10eee0f6b
Yusuf <yusluv1708@gmail.com>
2023-03-22 06:27:59 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - Checks for lowercase character
  * @c: The character is to be checked
- * Return : 1 for lowercased or 0 for anything else
+ * Return: 1 for lowercase or 0 for anything else
  */
 
 int _islower(int f)
